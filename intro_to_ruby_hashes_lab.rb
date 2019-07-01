e9fd059d0a2bb0c62522d4f04c5a0e2f9bc08d47
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-01 21:23:37 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,25 +1,42 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    name: 'Megan',
+    age: 26,
+    likes: 'me gitting gud @ ruby',
+    dislikes: 'ruby not gitting gud @ me'
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    name: 'Grace Hopper'
+  }
 end
 
-def id_generator
-  # return a hash with a key :id assigned to positive integer
+def id_generator 
+ id_gen = {
+   :id => 11
+   }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+
+  creator = {
+    key => value
+  }
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  
+  hash = {
+  }
+  
+if hash[key]
+ puts hash[key
 end
 
 def update_counting_hash(hash, key)
